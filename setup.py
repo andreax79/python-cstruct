3a0ea6efe513171b197713d78591e227e82eca30
Andrea Bonomi <andrea.bonomi@gmail.com>
2022-11-12 07:58:19 +0000

    setup

--- setup.py
+++ setup.py
@@ -1,43 +1,5 @@
-from setuptools import setup, find_packages
-from cstruct import __version__
+#!/usr/bin/env python
+import setuptools
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-setup(
-    name='cstruct',
-    version=__version__,
-    description="C-style structs for Python",
-    long_description="""\
-Convert C struct definitions into Python classes with methods for serializing/deserializing.""",
-    classifiers=[
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    keywords='struct',
-    author='Andrea Bonomi',
-    author_email='andrea.bonomi@gmail.com',
-    url='http://github.com/andreax79/python-cstruct',
-    license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=True,
-    install_requires=[
-        # -*- Extra requirements: -*-
-    ],
-    entry_points="""
-      # -*- Entry points: -*-
-      """,
-    test_suite='tests',
-    tests_require=['pytest'],
-)
+if __name__ == "__main__":
+    setuptools.setup()
